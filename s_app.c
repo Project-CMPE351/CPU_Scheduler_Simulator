12ea891b0a99db023d3ac508e3cf3265d6e82f12
dilnozrafeeq <59788767+dilnozrafeeq@users.noreply.github.com>
2023-01-03 22:28:34 +0200

    Update s_app.c

--- s_app.c
+++ s_app.c
@@ -11,12 +11,13 @@ typedef struct process {
     int arrival_time;
     int title;
     int priority;
+    int start_time;
     struct process *next;  
 } Process;
 
 static int num = 0; // Assign an id to a newly created process
 Process *createNode(int b_t, int a_t, int priority) {
-    Process *node = malloc(sizeof(Process));
+    Process *node = (Process*)malloc(sizeof(Process));
     node->burst_time = b_t;
     node->arrival_time = a_t;
     node->priority = priority;
@@ -25,7 +26,7 @@ Process *createNode(int b_t, int a_t, int priority) {
     node->next = NULL;
     return node;
 }
-Process *insertBack(Process *header, int b_t, int a_t, int priority) {
+Process *insertOrdered(Process *header, int b_t, int a_t, int priority) {
     Process *node = createNode(b_t, a_t, priority);
     if (header == NULL) {
         return node;
@@ -38,126 +39,93 @@ Process *insertBack(Process *header, int b_t, int a_t, int priority) {
     return header;
 }
 
-void error() {
-    printf("Required: -f <input.txt> | -o <output.txt>\n" );
-    exit(2);
-}
-int main (int argc, char *argv []) 
 
-
-{
-//Declarations
-    int inputOpt;
-    char f_flag = 0, o_flag=0;
-    char *out_fl = optarg;
-
-    if (argc<4){
-        error();
+void display(Process *header, FILE *output_file) {
+    Process *current = header;
+    printf("Scheduling Method: Shortest Job First –Preemptive\n");
+    printf("Process Waiting Times:\n");
+    int sum_wait_time = 0;
+    int count = 0;
+    while (current != NULL) {
+        int wait_time = current->start_time - current->arrival_time;
+        sum_wait_time += wait_time;
+        printf("P%d %d ms\n", current->title, wait_time);
+        current = current->next; //print title
+        count++;
     }
-    
-    while ((inputOpt = getopt(argc, argv, "f:o:")) != -1 ){
+    printf("Average Waiting Time: %.1f ms", (double) sum_wait_time / count);
+}
 
-        switch (inputOpt)
-        {
-        case 'f':
-            printf("f: %s\n", optarg);
-            f_flag = 1;
-            break;
-        case 'o':
-            printf("o: %s\n", optarg);
-            o_flag = 1;
-            char *out_fl = optarg; // define out_fl variable and assign value of optarg
-            break;
-        default:
-            printf("Invalid option: %c\n", inputOpt);
-            error();
-            break;
+void DoFCFS(Process *head){
+    Process *current = head;
+    int time_passed = 0;
+    while (true) {
+        time_passed += current->burst_time;
+        current = current->next;
+        if (current == NULL) break;
+        current->start_time = time_passed - current->arrival_time;
+    }
+}
+
+int main (int argc, char *argv []) 
+{
+    //Open input file
+    FILE *input_file = fopen("input.txt", "r");
+    if (input_file == NULL) {
+        printf("Error opening input file\n");
+        return 1;
+    }
+   
+    // Open output file
+    FILE *output_file = fopen("output.txt", "w");
+    if (output_file == NULL) {
+        printf("Error opening output file\n");
+        return 1;
+    }
+
+    // Read data from file and parse into linked list
+    Process *head = NULL;
+    char line[MAX_LINE_LENGTH];
+    char *token;
+
+
+
+    while (fgets(line, MAX_LINE_LENGTH, input_file) != NULL) {
+    // Create new node in linked list
+        Process *node = (Process*)malloc(sizeof(Process));
+
+        printf("%s", line);
+        if (head == NULL) {
+            head = node;
+            //current = head;
+            //
+        } else {
+            //current->next = node;
+            //current = current->next;
         }
 
-//Check if all options are given
-   if (!f_flag || !o_flag) {
-    printf("Both the -f and -o options are required\n");
-    error();
-}
 
- //Open input file
-FILE *input_file = fopen(optarg, "r");
-if (input_file == NULL) {
-printf("Error opening input file\n");
-return 1;
-}
-void display(Process *header, FILE *output_file) {
-  Process *current = header;
-  while (current != NULL) {
-    fprintf(output_file, "%d:%d:%d\n", current->title, current->burst_time, current->arrival_time, current->priority);
-    current = current->next;//print title
-  }
-}
-// Open output file
-FILE *output_file = fopen(out_fl, "w");
-if (output_file == NULL) {
-    printf("Error opening output file\n");
-    return 1;
-}
-
-// Read data from file and parse into linked list
-Process *head = NULL;
-Process *current = NULL;
-char line[MAX_LINE_LENGTH];
-char *delimiter = ":";
-char *token;
-
-
-while (fgets(line, MAX_LINE_LENGTH, input_file) != NULL) {
-// Create new node in linked list
-Process *node = malloc(sizeof(Process));
-if (head == NULL) {
-head = node;
-current = head;
-} else {
-current->next = node;
-current = current->next;
-}
-
-// Split line into fields and convert to appropriate data types
-{token = strtok(line, delimiter);
-node->burst_time = atoi(token);
-token = strtok(NULL, delimiter);
-node->arrival_time = atoi(token);
-token = strtok(NULL, delimiter);
-node->priority = atof(token);
-}
-
-        display(head);
     }
-// Close input file
-fclose(input_file);
+    // Close input file
+    fclose(input_file);
+
+    // algorithm goes here
+    DoFCFS(head);
+
+    // Iterate through linked list and write data to output file
+    display(head, output_file);
 
 
-// Open output file
-FILE *output_file = fopen(out_fl, "w");
-if (output_file == NULL) {
-    printf("Error opening output file\n");
-    return 1;
-}
+    // Close output file
+    fclose(output_file);
 
-// Iterate through linked list and write data to output file
-Process *current = head;
-while (current != NULL) {
-    fprintf(output_file, "%d:%d:%d\n", current->burst_time, current->arrival_time, current->priority);
-    current = current->next;
-}
-
-// Close output file
-fclose(output_file);
-
-// Iterate through linked list and free memory
-Process *current = head;
-while (current != NULL) {
-    Process *next = current->next;
-    free(current);
-    current = next;
-}
+    // Iterate through linked list and free memory
+    Process *cleaningPtr = head;
+    while (cleaningPtr != NULL) {
+        Process *next = cleaningPtr->next;
+        free(cleaningPtr);
+        cleaningPtr = next;
+    }
 }
 
 
